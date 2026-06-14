5807b25d02d64705acb8a7673e8a282fc29e8256
kinqbert <perehodko1010@gmail.com>
2026-06-14 15:09:31 +0300

    feat: added chirp filtering endpoint

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"sync/atomic"
 )
 
@@ -52,11 +54,89 @@ func main() {
 	mux.Handle("POST /admin/reset", cfg.resetFileserverHits())
 
 	mux.HandleFunc("GET /api/healthz", handleHealthz)
+	mux.HandleFunc("POST /api/validate_chirp", handleValidateChirp)
 
 	mux.Handle("/app/", http.StripPrefix("/app", cfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
 
 	log.Println("Starting server...")
 	log.Println(server.ListenAndServe())
+}
+
+type ValidateChirpBody struct {
+	Body string `json:"body"`
+}
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+func returnError(errorMessage string, w http.ResponseWriter) {
+	errorResponse := ErrorResponse{Error: errorMessage}
+
+	w.WriteHeader(http.StatusBadRequest)
+	errorResponseJson, err := json.Marshal(errorResponse)
+
+	if err != nil {
+		w.Write([]byte(""))
+	}
+
+	w.Write(errorResponseJson)
+}
+
+func filterProfanes(text string) string {
+	profanes := map[string]struct{}{
+		"kerfuffle": {},
+		"sharbert":  {},
+		"fornax":    {},
+	}
+
+	fmt.Printf("%s", text)
+
+	words := strings.Fields(text)
+
+	for i, word := range words {
+		normalized := strings.ToLower(word)
+
+		if _, isProfane := profanes[normalized]; isProfane {
+			words[i] = "****"
+		}
+	}
+
+	return strings.Join(words, " ")
+}
+
+func handleValidateChirp(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	var body ValidateChirpBody
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&body)
+
+	if err != nil {
+		returnError("Something went wrong", w)
+		return
+	}
+
+	if len(body.Body) > 140 {
+		returnError("Chirp is too long", w)
+		return
+	}
+
+	cleanedChirp := filterProfanes(body.Body)
+
+	successResponse := struct {
+		CleanedBody string `json:"cleaned_body"`
+	}{CleanedBody: cleanedChirp}
+	encodedResponse, err := json.Marshal(successResponse)
+
+	if err != nil {
+		returnError("Something went wrong", w)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(encodedResponse)
 }
 
 func handleHealthz(w http.ResponseWriter, r *http.Request) {
